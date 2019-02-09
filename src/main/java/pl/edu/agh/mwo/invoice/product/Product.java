160531b95489d62286519c0fe0056dc247970e79
Student14 <Student14@s10n-mwo-3.pcoip.ki.agh.edu.pl>
2019-02-09 11:12:19 +0100

    Udało sie

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,33 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if(name == null) {
+			throw new IllegalArgumentException("Name connot be null.");
+		}
+		if(name == "") {
+			throw new IllegalArgumentException("Name connot be null.");
+		}
+		if(price == null) {
+			throw new IllegalArgumentException("Price connot be null.");
+		}
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
 	}
 
 	public String getName() {
-		return null;
+		return this.name;
 	}
 
 	public BigDecimal getPrice() {
-		return null;
+		return this.price;
 	}
 
 	public BigDecimal getTaxPercent() {
-		return null;
+		return this.taxPercent;
 	}
 
 	public BigDecimal getPriceWithTax() {
-		return null;
+		return this.price.multiply(this.taxPercent).add(this.price);
 	}
 }
